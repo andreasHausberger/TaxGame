5d3d28fd5821d060b82ae1408af2a2876513ebd0
andreasHausberger <hausbergerandreas1@gmail.com>
2018-11-27 13:16:39 +0100

    added flag in index router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("Trying to render index page");
+  res.render('../public/generated/signup.ejs', { title: 'Express' });
 });
 
 module.exports = router;
