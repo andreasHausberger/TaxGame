5801e9e3bd227e213867b84b586632947d8b7032
andreasHausberger <hausbergerandreas1@gmail.com>
2018-12-01 18:08:26 +0100

    fewer requirements for email.

--- database/mdb.js
+++ database/mdb.js
@@ -4,8 +4,6 @@ var bcrypt = require('bcryptjs');
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
-        unique: true,
-        required: true,
         trim: true
     },
     username: {
